27bc02f228c22c78bf85dc06a3ff9ba58b4d8072
Valerie Lemuth <valerie.lemuth@uni-weimar.de>
2018-04-26 20:09:02 +0200

    Add function for creating new html document but extracting the letters doesn't work yet

--- HtmlPython.py
+++ HtmlPython.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
 import os
+import datetime
+from webbrowser import open_new_tab
 
 def findDate():
 
@@ -10,10 +12,32 @@ def findDate():
             filename = os.path.join(direc, file)
             with open(filename, "r") as openfile:
                 soup = BeautifulSoup(openfile, 'html.parser')
+            
+            for date in soup.find_all("h3"):                    #searches for the date but problem: only files with date at the top get searched
+                for number in soup.find_all("h4"):
+                    #for day in soup.find_all("p", attrs={"class": "center"}):      #way to find the individual days
+                    intoHtml(date.text, number.text, "The Letter")
 
-            for time in soup.find_all("h3"):                #exchange h3 for any tag you are looking for (should work...I hope...)
-                print(time.text)                            #for now it only prints the text of the tag but it's a start
+def intoHtml(date, number, body):
+
+    filename = date + "  " + number + '.html'
+    f = open(filename, 'w')
+
+    #the %s is a placeholder for a variable --> they are defined below
+    wrapper= """<html>
+    <head>
+    <title>%s, %s </title>                  
+    </head>
+    <body><p>%s</p></body>
+    </html>"""
+
+    #definition for %s
+    whole = wrapper % (date, number, body)      #depending on the order e.g. date goes in place of first %s and so on
+    f.write(whole)
+    f.close()
+
+    filename = "/Letters/" + filename       #Should put it in directory Letters but it doesn't... don't know why
         
-
+    open_new_tab(filename)
 
 findDate()
