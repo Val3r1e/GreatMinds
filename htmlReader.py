94a8aa8a3e3d0700c5a10763a88d703b26ac40a7
Johanna Sacher <johanna.sacher@uni-weimar.de>
2018-04-30 20:21:48 +0200

    works for both charlotte stein folders 🧙‍♀️

--- htmlReader.py
+++ htmlReader.py
@@ -8,13 +8,12 @@ from webbrowser import open_new_tab
 import sys
 
 def extract(direc):
-    """I left all the print-statements in it so it'll be easier for us to adapt the code to the other folders. If we don't need them
-    eventually, we can throw them all out of course"""
-
-    #direc = "../data/briefe-an-charlotte-stein-band-1"         #add your directory
+    """I left all the print-statements in it so it'll be easier 
+    for us to adapt the code to the other folders. 
+    If we don't need them eventually, we can throw them all out of course"""
 
     fileindex = 1
-    date = "XXXX"                                    #in case there is no date
+    year = "XXXX"                                    #in case there is no date
 
     for file in os.listdir(direc):
         if file.endswith(".html"):
@@ -26,50 +25,59 @@ def extract(direc):
 
                 body = ""
                 number = "YYY"
+                date = ""
                 h4_counter = 0
-                document = soup.find_all(["h3","h4","p"])
+                document = soup.find_all(["h3","h4", "h5","p"])
                 #print(document)
                 #print (type(numbers))
 
                 for element in document:
                     if element.name == "h3":
-                        date = element.text
+                        year = element.text
 
                     elif element.name == "h4":
                         if h4_counter != 0:
                             #print("Jetzt schreibe ich Nummer ", number)
-                            intoHtml(date, number, body)
+                            intoHtml(year, number, date, body)
                             body = ""
                             #print("Counter: ", h4_counter)
 
                         number = element.text
                         h4_counter += 1
                         #print(number)
+                    elif element.name == "h5":
+                        date = element.text
 
                     elif element.name == "p":
                         body += "<br>"+element.text
 
                 if h4_counter != 0:
                     #print("Jetzt schreibe ich Nummer ", number)
-                    intoHtml(date, number, body)
+                    intoHtml(year, number, date, body)
                     body = ""
                     #print("Counter: ", h4_counter)
 
-def intoHtml(date, number, body):
+def intoHtml(year, number, date, body):
 
-    filename = date + " - " + number + '.html'
-    f = open("ignore/letters/"+filename, 'w')
+    filename = year + " - " + number + '.html'
+    
+    """instead of 'Stein1' in the end put the folder where you want to 
+    collect those bunch of letters :)"""
+
+    f = open("ignore/letters/Stein1"+filename, 'w') 
 
     #the %s is a placeholder for a variable --> they are defined below
     wrapper= """<html>
     <head>
     <title>%s, %s </title>
     </head>
-    <body><h4>%s - %s</h4><p>%s</p></body>
+    <body><h4>%s - %s</h4>
+    <h5>%s</h5>
+    <p>%s</p></body>
     </html>"""
 
     #definition for %s
-    whole = wrapper % (date, number, date, number, body)      #depending on the order e.g. date goes in place of first %s and so on
+    whole = wrapper % (year, number, year, number, date, body)      #depending on the order e.g. date goes in place of first %s and so on
     f.write(whole)
     f.close()
 
