edef809e9af8387c167383801ba8ac447f2ccd2f
Johanna Sacher <johanna.sacher@uni-weimar.de>
2018-06-01 15:41:02 +0200

    fixed the csv data :)

--- CsvData/get_all_dates.py
+++ CsvData/get_all_dates.py
@@ -11,10 +11,11 @@ import collections
 def getData(rootdir):
     """gets the year and the date from the metadatafiles (.json files)"""
 
-    datesSchiller = []
-    datesStein = []
+    #datesSchiller = []
+    #datesStein = []
     yearsSchiller = []
     yearsStein = []
+    yearsChristiane = []
 
     for subdir, dirs, files in os.walk(rootdir):
         for file in files:
@@ -23,11 +24,11 @@ def getData(rootdir):
 
                 if "schiller" in config["Collection"]:
 
-                    if "Date" in config:
-                        datesSchiller.append(config["Date"])
-                        date = str(config["Date"])
-                        date = date.split(" ")
-                        # years.append(date[-1])
+                    # if "Date" in config:
+                    #     datesSchiller.append(config["Date"])
+                    #     date = str(config["Date"])
+                    #     date = date.split(" ")
+                    #     # years.append(date[-1])
 
                     if "Year" in config:
                         year = config["Year"]
@@ -35,16 +36,28 @@ def getData(rootdir):
                             yearsSchiller.append(config["Year"])
 
                 elif "stein" in config["Collection"]:
-                    if "Date" in config:
-                        datesStein.append(config["Date"])
-                        date = str(config["Date"])
-                        date = date.split(" ")
-                        # years.append(date[-1])
+                    # if "Date" in config:
+                    #     datesStein.append(config["Date"])
+                    #     date = str(config["Date"])
+                    #     date = date.split(" ")
+                    #     # years.append(date[-1])
 
                     if "Year" in config:
                         year = config["Year"].strip()
                         if year.isdigit():
                             yearsStein.append(year)
+
+                elif "seiner" in config["Collection"]:
+                    # if "Date" in config:
+                    #     datesStein.append(config["Date"])
+                    #     date = str(config["Date"])
+                    #     date = date.split(" ")
+                    #     # years.append(date[-1])
+
+                    if "Year" in config:
+                        year = config["Year"].strip()
+                        if year.isdigit():
+                            yearsChristiane.append(year)
                 else:
                     pass
             else:
@@ -52,31 +65,42 @@ def getData(rootdir):
 
     counterSchiller = Counter(yearsSchiller)
     counterStein = Counter(yearsStein)
+    counterChristiane = Counter(yearsChristiane)
     #pprint(counterStein)
 
     orderedSchiller = collections.OrderedDict(sorted(counterSchiller.items()))
     orderedSchiller = list(orderedSchiller.items())
+    #pprint(orderedSchiller)
 
     orderedStein = collections.OrderedDict(sorted(counterStein.items()))
     orderedStein = list(orderedStein.items())
+    #pprint(orderedStein)
+
+    orderedChristiane = collections.OrderedDict(sorted(counterChristiane.items()))
+    orderedChristiane = list(orderedChristiane.items())
+    #pprint(orderedChristiane)
 
     all_years = []
-    all_data = [tuple(["Year"] + ["NumberSchiller"] + ["NumberStein"])]
+    all_data = [tuple(["Year"] + ["NumberSchiller"] + ["NumberStein"] + ["NumberChristiane"])]
 
     for x in orderedSchiller:
         if x[0] not in all_years:
             all_years.append(x[0])
-    for y in orderedStein:
-        if y[0] not in all_years:
-            all_years.append(y[0])
+    for x in orderedStein:
+        if x[0] not in all_years:
+            all_years.append(x[0])
+    for x in orderedChristiane:
+        if x[0] not in all_years:
+            all_years.append(x[0])
     
     all_years.sort()
-    print(all_years)
+    #print(all_years)
 
     for x in all_years:
         Schiller_c = next((b for a, b in orderedSchiller if a == x), 0)
         Stein_c = next((d for c, d in orderedStein if c == x), 0)
-        all_data.append(tuple([str(x)] + [str(Schiller_c)] + [str(Stein_c)]))
+        Christiane_c = next((d for c, d in orderedChristiane if c == x), 0)
+        all_data.append(tuple([str(x)] + [str(Schiller_c)] + [str(Stein_c)] + [str(Christiane_c)]))
 
     pprint(all_data)
     into_csv(all_data)
